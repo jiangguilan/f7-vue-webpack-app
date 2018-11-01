681751462d081b2a95d74f80e1d037e5ff4a8b3a
Lucy <Lucy@mail.boxintheship.com>
2018-11-01 18:09:35 +0800

    s

--- src/pages/reset_password.vue
+++ src/pages/reset_password.vue
@@ -6,25 +6,43 @@
     </div>
     <div class="rp_body">
       <div class="rp_top">
-        <span>Email</span>
-        <span>Mobile phone</span>
+        <span @click="re_email">
+          Email
+          <div v-show="isEmail"></div>
+        </span>
+        <span @click="re_phone">
+          Mobile phone
+          <div v-show="isPhone" class="rp_phone_deg"></div>
+        </span>
       </div>
-      <div class="rp_bottom">
-        <div>
-          <span>Mobile Phone</span>
+      <div v-show="isPhone" class="rp_phone">
+        <div class="rp_bottom">
           <div>
-            <span>+60</span>
-            <input type="text">
+            <span>Mobile Phone</span>
+            <div>
+              <span>+60</span>
+              <input type="text">
+            </div>
           </div>
-        </div>
-        <div>
-          <span>SMS code</span>
           <div>
-            <input type="text">
-            <span>Send SMS</span>
+            <span>SMS code</span>
+            <div>
+              <input class="rp_code_input" type="text">
+              <span class="rp_send">Send SMS</span>
+            </div>
           </div>
+          <f7-link class="rp_submit">Reset Password</f7-link>
         </div>
-        <f7-link>Reset Password</f7-link>
+      </div>
+      <div v-show="isEmail" class="rp_email">
+        <div class="rp_email_box">
+          <div>
+            <span>Email</span>
+            <input type="text">  
+          </div>
+          <p>Please check your email and complete the verification process to reset your password</p>
+        </div>
+        <f7-link class="rp_submit">Reset Password</f7-link>
       </div>
     </div>
   </f7-page>
@@ -33,14 +51,22 @@
 export default {
     data: function () {
       return {
-        
+        isPhone:false,
+        isEmail:true,
       }
     },
     methods: {
       rp_back:function(){
         window.history.go(-1)
-      }
-     
+      },
+      re_email:function(){
+        this.isEmail=true;
+        this.isPhone=false;
+      },
+      re_phone:function(){
+        this.isEmail=false;
+        this.isPhone=true;
+      },
     },
     mounted: function () {
       var $$=this.Dom7;
